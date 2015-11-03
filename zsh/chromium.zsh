ff58bb859c34778ac50d38c670ccac933b8f7b22
Anton Klautsan <dukexar@gmail.com>
2015-11-03 06:33:29 +0000

    Add Chromium patsh

--- zsh/chromium.zsh
+++ zsh/chromium.zsh
@@ -1,4 +1,6 @@
 export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
+export GYP_CHROMIUM_NO_ACTION=1
+export GYP_DEFINES='component=shared_library dcheck_always_on=1'
 
 path=(
     ~/src/depot_tools
